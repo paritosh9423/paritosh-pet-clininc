f369833e2167d1237f1d27eac58366df60ded071
paritoshP <paritosh.pradeep@rediffmail.com>
2018-10-28 20:30:04 +0530

    CID-000

---
+++ src/main/java/com/petclinic/paritoshpetclinic/ParitoshPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.petclinic.paritoshpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParitoshPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParitoshPetClinicApplication.class, args);
+    }
+}
