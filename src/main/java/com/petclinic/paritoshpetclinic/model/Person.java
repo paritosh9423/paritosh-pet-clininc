6a8ae13097971bd46a5e0f79f0cb2c5713f52292
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-08 14:25:07 +0530

    CID-001 -Added basic POJOs . Closes #2 .

---
+++ src/main/java/com/petclinic/paritoshpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package com.petclinic.paritoshpetclinic.model;
+//Created by ppradeep on 08/11/18, 2:15 PM
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
