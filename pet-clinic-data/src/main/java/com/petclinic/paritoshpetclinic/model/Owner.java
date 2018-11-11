c4fe77e79340441f8d7e1cb763062702743169af
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-11 15:11:44 +0530

    CID-021 Addeed visit and updated owner with pet entities. Closes #26

--- pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/model/Owner.java
@@ -1,5 +1,17 @@
 package com.petclinic.paritoshpetclinic.model;
 //Created by ppradeep on 08/11/18, 2:17 PM
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> petsSet  ;
+
+    public Set<Pet> getPetsSet() {
+        return petsSet;
+    }
+
+    public void setPetsSet(Set<Pet> petsSet) {
+        this.petsSet = petsSet;
+    }
 }
