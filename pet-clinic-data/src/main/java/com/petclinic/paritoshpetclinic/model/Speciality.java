cf18f905e0030bad5793d8baf471557903cac709
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-11 15:24:23 +0530

    CID-022 Addeed speciality and associated to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/model/Speciality.java
@@ -0,0 +1,15 @@
+package com.petclinic.paritoshpetclinic.model;
+//Created by ppradeep on 11/11/18, 3:14 PM
+
+public class Speciality extends  BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/model/Vet.java
@@ -1,5 +1,17 @@
 package com.petclinic.paritoshpetclinic.model;
 //Created by ppradeep on 08/11/18, 2:17 PM
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
