123dc131be52940d45f1423f2ebd977b0ca04676
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-11 16:32:04 +0530

    CID-027 Added speciality service and specialityservice map Closes #31

---
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.petclinic.paritoshpetclinic.services;
+
+import com.petclinic.paritoshpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.petclinic.paritoshpetclinic.services.map;
+//Created by ppradeep on 11/11/18, 4:24 PM
+
+import com.petclinic.paritoshpetclinic.model.Speciality;
+import com.petclinic.paritoshpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deletebyID(Long id) {
+        super.deleteByID(id);
+    }
+}
