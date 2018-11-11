46189a6a185f088565b3f9b1e157d44b4f9c7d1d
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-11 15:52:16 +0530

    CID-025 Added pettype gto bootstrap. Closes #32

--- pet-clinic-web/src/main/java/com/petclinic/paritoshpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/paritoshpetclinic/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package com.petclinic.paritoshpetclinic.bootstrap;
 //Created by ppradeep on 10/11/18, 1:14 PM
 
 import com.petclinic.paritoshpetclinic.model.Owner;
+import com.petclinic.paritoshpetclinic.model.PetType;
 import com.petclinic.paritoshpetclinic.model.Vet;
 import com.petclinic.paritoshpetclinic.services.OwnerService;
+import com.petclinic.paritoshpetclinic.services.PetTypeService;
 import com.petclinic.paritoshpetclinic.services.VetService;
 import com.petclinic.paritoshpetclinic.services.map.OwnerServiceMap;
 import com.petclinic.paritoshpetclinic.services.map.VetServiceMap;
@@ -16,17 +18,29 @@ public class DataLoader implements CommandLineRunner {
 
     private  final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired//Not required to write autowired in constructor injectin n spring 5 but is preferred to write for understanding purpose.
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(dog);
+
         System.out.println("Start Loading Owners");
 
         Owner o1 = new Owner();
