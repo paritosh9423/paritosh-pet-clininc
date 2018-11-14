2f8c521cbda49087382f4ecf1901066c0bfe6548
paritoshP <paritosh.pradeep@rediffmail.com>
2018-11-14 20:46:32 +0530

    CID-041 Added map service for Visit. Closes #46

---
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.petclinic.paritoshpetclinic.services;
+//Created by ppradeep on 14/11/18, 8:36 PM
+
+import com.petclinic.paritoshpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/petclinic/paritoshpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,40 @@
+package com.petclinic.paritoshpetclinic.services.map;
+//Created by ppradeep on 14/11/18, 8:37 PM
+
+import com.petclinic.paritoshpetclinic.model.Visit;
+import com.petclinic.paritoshpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findByID(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() ==null ||object.getPet().getId()==null
+        ||object.getPet().getOwner().getId()==null)
+                throw new RuntimeException("Invalid visit!");
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deletebyID(Long aLong) {
+        super.deleteByID(aLong);
+    }
+}
