a633518808d9ed134c47a3b2bb29397ccffeb9bf
Paritosh <paritosh.9423@gmail.com>
2018-12-11 20:57:12 +0530

    Testing Commit

--- pet-clinic-web/src/main/java/com/petclinic/paritoshpetclinic/ParitoshPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/petclinic/paritoshpetclinic/ParitoshPetClinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParitoshPetClinicApplication {
 
     public static void main(String[] args) {
+    	
         SpringApplication.run(ParitoshPetClinicApplication.class, args);
     }
 }
